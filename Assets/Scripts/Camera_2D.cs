b4a3bb4d62ff1d0f9389c95e1868d4691a392b71
slamerly <97894251+slamerly@users.noreply.github.com>
2022-10-13 18:02:01 +0200

    base

---
+++ Assets/Scripts/Camera_2D.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_2D : MonoBehaviour
+{
+    public GameObject target;
+    public float x, y;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position;
+        transform.localPosition += new Vector3(-x, y, -x); 
+        transform.LookAt(target.transform);
+        
+    }
+}
